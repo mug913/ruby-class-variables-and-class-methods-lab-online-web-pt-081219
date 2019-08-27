07d45b160020f816aeb749e013edc552b8f9fdce
Brad Alexander <brad.alexander913@gmail.com>
2019-08-27 01:30:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song 
+  
+  @@count = 0 
+  @@artist_count = {}
+  @@genres = []
+  
+  def count 
+    @@count 
+  end 
+  
+  def artists 
+    @@artist_name 
+  end 
+  
+  def genre_count 
+    @@genres 
+  end 
+  
+  def initialize(name,artist,genre)
+    @@count =+ 1 
+    if !@@genres.contain?(genre)
+      @@genres << genre 
+    end 
+    
+    
+  end 
+  
+  attr_accessor :name, :artist, :genre
+  
+  
+end 
