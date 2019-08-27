e68f70af2ac56414faf483e9f4637c28cb38b7a2
Brad Alexander <brad.alexander913@gmail.com>
2019-08-27 01:43:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,19 +26,18 @@ class Song
     @artist = artist 
     @genre = genre
     
-    if !@@genres.include?(genre)
-      @@genres << genre
-      @@genre_count[genre] = 1 
+    if @@genres.include?(genre)
+       @@genre_count[genre] += 1
     else 
-      @@genre_count[genre] += 1
+      @@genres << genre
+      @@genre_count[genre] = 1  
     end 
     
-     if !@@artists.include?(artist)
-       binding.pry
+    if @@artists.include?(artist)
+       @@artist_count[artist] += 1
+    else 
       @@artists << artist 
       @@artist_count[artist] = 1
-    else 
-       @@artist_count[artist] += 1
     end 
     
   end 
